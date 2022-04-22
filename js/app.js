5f20f3dbfdc045e7129cd5b99eb80024c5f208f5
jdelinila <johnrobertdelinila@dict.gov.ph>
2022-04-23 03:33:21 +0800

    new commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 // AUTHENTICATION
+
+const facebook_login=()=>{
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then(function(result) {
+       var user=result.user;
+       window.location.replace('home.html')
+       console.log("user===>",user.displayName)
+        
+      }).catch(function(error) {
+        alert("error===>",error.message);
+        console.log(error.message);
+      });
+}
+
+
+const logout=()=>{
+  firebase.auth().signOut()
+  .then(()=>{
+   window.location.replace("index.html")
+  }
+  )
+  .catch(()=>{
+
+  }
+  )
+}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 // FIRESTORE
+
+function chat(docs) {
+  var area=document.getElementById('areat');
+  var dd=document.createElement("div");
+  var head=document.createElement('h6');
+  var btnd=document.createElement("button");
+  btnd.setAttribute("class","btn btn-danger deleteBtn");
+
+  var btt=document.createTextNode("Delete");
+  btnd.setAttribute("onclick","deleteitem(this)");
+  btnd.setAttribute("id", docs.id);
+  btnd.appendChild(btt);
+  
+  var ct = docs.data().email + ": " + docs.data().chattext;
+  var jj=document.createElement('p');
+  jj.setAttribute("class","para");
+
+  area.appendChild(jj);
+  
+  var text=document.createTextNode(ct);
+
+  head.appendChild(text);
+  
+  head.className="edit";
+  dd.appendChild(head);
+  dd.appendChild(btnd);
+
+  dd.className="wwa";
+  area.appendChild(dd);
+}
+
+
+function enter(){
+    var input = document.getElementById('mess');
+    var chat= {
+      chattext: mess.value,
+      email: firebase.auth().currentUser.email
+    }
+    
+    mess.value="";
+}
+
+function deleteitem(e){
+    e.parentNode.remove();
+}
+
